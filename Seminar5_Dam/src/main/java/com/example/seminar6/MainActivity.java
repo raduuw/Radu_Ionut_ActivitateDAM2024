f1e7de64a583718a66ac4bcd68619e65eed45df4
Radu <radudrop1@gmail.com>
2025-01-20 11:20:02 +0200

    Seminar 5 commit 5

---
+++ Seminar5_Dam/src/main/java/com/example/seminar6/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.seminar6;
+
+import android.os.Bundle;
+
+
+import android.content.Intent;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Masina> listaMasina = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.buttonAdaugaMasina);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), AddMasinaActivity.class);
+                startActivityForResult(it, 203);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button listaMasiniBtn = findViewById(R.id.buttonListaMasini);
+        listaMasiniBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), MasinaListActivity.class);
+                it.putParcelableArrayListExtra("masini", (ArrayList<? extends Parcelable>) listaMasina);
+                startActivity(it);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 203 && resultCode == RESULT_OK && data != null) {
+            Masina masina = data.getParcelableExtra("masina");
+            listaMasina.add(masina);
+        }
+    }
+}
