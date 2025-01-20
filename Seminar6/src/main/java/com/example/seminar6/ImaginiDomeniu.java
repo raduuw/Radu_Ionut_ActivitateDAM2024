a9027e6a3a7226a1ab2103cf7cb85ab4df25a8a4
Radu <radudrop1@gmail.com>
2025-01-20 11:55:55 +0200

    Seminar 6 commit 6

---
+++ Seminar6/src/main/java/com/example/seminar6/ImaginiDomeniu.java
@@ -0,0 +1,39 @@
+package com.example.seminar6;
+
+import android.graphics.Bitmap;
+
+public class ImaginiDomeniu {
+    private String textAfisat;
+    private Bitmap imagine;
+    private String link;
+
+    public ImaginiDomeniu(String textAfisat, Bitmap imagine, String link) {
+        this.textAfisat = textAfisat;
+        this.imagine = imagine;
+        this.link = link;
+    }
+
+    public String getTextAfisat() {
+        return textAfisat;
+    }
+
+    public void setTextAfisat(String textAfisat) {
+        this.textAfisat = textAfisat;
+    }
+
+    public Bitmap getImagine() {
+        return imagine;
+    }
+
+    public void setImagine(Bitmap imagine) {
+        this.imagine = imagine;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
